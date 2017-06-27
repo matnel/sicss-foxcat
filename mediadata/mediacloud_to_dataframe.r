1c8891abeff8367524ca6d8284936bfa52be3004
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-06-27 11:24:35 -0400

    Make media sources as factors

--- mediadata/mediacloud_to_dataframe.r
+++ mediadata/mediacloud_to_dataframe.r
@@ -6,6 +6,9 @@ data <- fromJSON( 'collected.json', simplifyDataFrame=T )
 data$publish_date <- as.POSIXct( data$publish_date, format="%Y-%m-%d %H:%M:%S" )
 data$collect_date <- as.POSIXct( data$collect_date, format="%Y-%m-%d %H:%M:%S" )
 
+data$media_name <- as.factor( data$media_name )
+
+
 ## basic descriptives
 table( data$media_name )
 
