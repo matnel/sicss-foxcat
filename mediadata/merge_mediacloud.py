32ca53d4de2328c681063e92705b75145f7d543a
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-06-26 21:27:12 -0400

    Merge all separatet monthly json files to one larger file

---
+++ mediadata/merge_mediacloud.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+megadata = []
+
+files = filter( lambda x: 'metadata_' in x, os.listdir( '.' ) )
+
+for f in files:
+
+    metadata = json.load( open( f ) )
+    wordmatrix = json.load( open( f.replace('metadata', 'wordmatrix') ) )
+
+    data = {}
+
+    for m in metadata:
+        data[ m['stories_id'] ] = m
+
+    all_words = wordmatrix['word_list']
+
+    for stories_id, words in wordmatrix['word_matrix'].items():
+
+        full_text = ''
+        for wid, wcount in words.items():
+
+            word = all_words[ int( wid ) ][0] + ' '
+            word = wcount * word
+            full_text += word
+
+        data[ int( stories_id ) ]['full_text_bow'] = full_text
+
+    megadata += data.values()
+
+print 'Total of', len( megadata )
+
+json.dump( megadata, open('collected.json', 'w') )
