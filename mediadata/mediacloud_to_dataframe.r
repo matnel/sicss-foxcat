56d16b15595201e93a9592482e48d06144aec792
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-06-26 21:42:52 -0400

    First version of converting JSON to robject

---
+++ mediadata/mediacloud_to_dataframe.r
@@ -0,0 +1,12 @@
+library('jsonlite')
+
+data <- fromJSON( 'collected.json', simplifyDataFrame=T )
+
+## clean up some variables
+data$publish_date <- as.POSIXct( data$publish_date, format="%Y-%m-%d %H:%M:%S" )
+data$collect_date <- as.POSIXct( data$collect_date, format="%Y-%m-%d %H:%M:%S" )
+
+## basic descriptives
+table( data$media_name )
+
+save( data, file = 'collected.rdata' )
